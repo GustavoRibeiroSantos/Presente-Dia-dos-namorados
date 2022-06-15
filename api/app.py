06d27149f07d0d9588e9937d3c7c9985f69c15eb
Gustavo Ribeiro <gurst6@gmail.com>
2022-06-15 19:46:43 -0300

    Project upload

--- api/app.py
+++ api/app.py
@@ -24,11 +24,9 @@ def sms_reply():
 
     resp = MessagingResponse()
     # Create reply
-    resp.message("Oi Siclana, tudo bem? Meu nome é Casé, sou o bot que o Fulano criou para te lembra o quão importante você para ele! :)")
-    resp.message("Ele só quer que você saiba que ele te amo muito, e você é umas das coisas mais maravilhosa que aconteceu na vida dele")
-    resp.message("Ele me desenvolveu especialmente para esse dia! <3")
+    resp.message("YOUR MESSAGE")
 
-    frases = ["Te amo infinitamente mais mor!","Que sorte eu tenho de ter você cmg!","Espero que cada dia mais eu possa ser feliz com você!", "E só espero casar com você e ter o Gustavo junior <3"]
+    frases = ["YOUR MESSAGE"]
     fotos = ['PICTURES ARRAY']
     for x in range(0, 4):
         client.messages.create(body=frases[x],
@@ -36,12 +34,10 @@ def sms_reply():
                             from_=from_whatsapp_number,
                             to=to_whatsapp_number)
         
-    resp.message("O que você gostaria de ver hoje?")
-
-    todas_fotos = 'PICTURES'
+    todas_fotos = 'YOUR PICTURES'
     resp.message(f"Fotos de vocês dois: {todas_fotos}")
 
-    musicas = 'YOUr PLAYSLIST'
+    musicas = 'YOUR PLAYSLIST'
     resp.message(f"Músicas que fazem ele lembrar de você: {musicas}")
 
     return str(resp)
